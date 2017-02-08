723491ff3e480221d02d54d7c77e3e2792a233c1
4015b16 <4015b16@icaco.ece.uprm.edu>
2017-02-08 10:56:45 -0400

    Se coloco color en el fondo, centralizo un ovalo en el frame y se crearon bordes

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
+		
 		JFrame myFrame = new JFrame("Jose A. Velazquez Torres");
+		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		myFrame.setLocationRelativeTo(null);
 	}
