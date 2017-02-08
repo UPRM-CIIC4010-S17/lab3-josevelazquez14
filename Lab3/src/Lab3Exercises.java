5a8d2d7ced11525357386fa2828f482d08a08248
4015b16 <4015b16@icaco.ece.uprm.edu>
2017-02-08 10:11:14 -0400

    Se le cambio el titulo al JFrame, se cambio su tama~o y se centralizo en la pantalla.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jose A. Velazquez");
+		JFrame myFrame = new JFrame("Jose A. Velazquez Torres");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
